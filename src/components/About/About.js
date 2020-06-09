6c49bab4613fdf771ee48938b14336e1e1930888
Clever Mukori <cmukori@learnorteach.com>
2020-06-09 13:20:53 -0400

    all done

---
+++ src/components/About/About.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render (){
+        return(
+            <div>
+                <h2>About</h2>
+                <p>Welcome to the React Actors corner, where we make code do what we want</p>
+            </div>
+
+        );
+
+    }
+}
+
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>1-800-CASH-NOW</p>
+                <p>react@reactactor.com</p>
+            </div>
+        );
+    }
+}
+
+
+export default Contact;
